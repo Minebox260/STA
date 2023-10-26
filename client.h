44bb01a79c5d36525349a352950bd2ce6ac64419
NawidF <124148152+NawidF@users.noreply.github.com>
2023-10-26 11:31:47 +0200

    Add files via upload

---
+++ client.h
@@ -0,0 +1,37 @@
+#ifndef _SERVEUR_H
+    #define _SERVEUR_H
+    #define _GNU_SOURCE
+    
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <sys/types.h>          
+    #include <sys/socket.h>
+    #include <sys/wait.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+    #include <pthread.h>
+    
+    #define MAXOCTETS 150
+    #define MAXVOITURES 2
+
+    #define CHECK_ERROR(val1,val2,msg)   if (val1==val2) \
+                                        { perror(msg); \
+                                            exit(EXIT_FAILURE); }
+
+    void * user_menu(void * t_data);
+    void handle_input(char * menu_input);
+
+    extern static int sd; //socket de dialogue
+
+    typedef struct car {
+        char ip[MAXOCTETS];
+        int pos_x;
+        int pos_y;
+    } car;
+
+    extern struct car * cars[MAXVOITURES];
+
+#endif
